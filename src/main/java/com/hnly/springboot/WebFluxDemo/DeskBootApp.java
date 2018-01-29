4f8300c306734153223625ba9766931f5bd17b90
ligengjun <hnlylgj@qq.com>
2018-01-29 16:47:07 +0800

    20180129-5-master

---
+++ src/main/java/com/hnly/springboot/WebFluxDemo/DeskBootApp.java
@@ -0,0 +1,31 @@
+package com.hnly.springboot.WebFluxDemo;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+import org.springframework.boot.SpringApplication;  
+import org.springframework.boot.autoconfigure.SpringBootApplication;  
+
+@SpringBootApplication  
+public class DeskBootApp 
+{  
+
+	
+	
+    public static void main(String[] args) 
+    {  
+        SpringApplication.run(DeskBootApp.class, args);  
+       //System.out.println( " SpringApplication.runing......" );
+        
+        
+    } 
+    public static void FormStarter(String[] args)
+    {  
+    	      
+    	 main(args); 
+        //SpringApplication.run(DeskSpringBootApp.class, args);  
+       
+        
+        
+    }  
+}  
